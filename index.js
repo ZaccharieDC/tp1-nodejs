bd6f3a72366ea0c8ca06860c88a759b9cc9eafd5
ZaccharieDC <zaccharie.dicristofaro@gmail.com>
2022-03-24 13:43:28 +0100

    Feat: Ajout des cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
 const users = require('./route')
 
+app.use(cors({
+    origin: ['http://localhost:63342']
+}))
 app.use(express.json())
 app.use('/users', users)
 
